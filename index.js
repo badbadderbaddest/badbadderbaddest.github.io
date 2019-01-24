83ca0d567d4e3bcd074aa4c9d73189948533984d
badbadderbaddest <badbadderbaddest@hack.none>
2019-01-24 08:36:34 -0800

    Add index

---
+++ index.js
@@ -0,0 +1,16 @@
+//{ stubs
+
+if (typeof console === "undefined") {
+    console = {
+        log: function () { },
+        info: function () { },
+        warn: function () { },
+        error: function () { }
+    };
+}
+
+//}
+
+//{ init
+
+//}
